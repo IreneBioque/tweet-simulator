8d24cfab4cc0010b7d5cfa841f7f6abc8b4f402d
IreneBioque <irenebioque@gmail.com>
2022-01-18 20:23:02 +0100

    add reload state and ls

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import { Container, Snackbar} from "@material-ui/core"
 import Header from "./components/Header";
 import SendTweet from "./components/SendTweet";
+import ListTweets from "./components/ListTweets";
 
 import {TWEETS_STORAGE} from './utils/constants';
 
@@ -11,17 +12,34 @@ const [toastProps, setToastProps] = useState({
   text: null
 })
 const [allTweets, setAllTweets] = useState([]);
+const [reloadTweets, setReloadTweets] = useState(false)
 
 useEffect(()=>{
+  // getItem para recuperar información
   const AllTweetsStorage = localStorage.getItem(TWEETS_STORAGE);
+  // parse convierte una cadena de texto JSON en un objeto JavaScript
   const AllTweetsArray = JSON.parse(AllTweetsStorage);
+  console.log(JSON.parse(AllTweetsStorage))
   setAllTweets(AllTweetsArray);
-}, [])
+  setReloadTweets(false)
+}, [reloadTweets])
+// AL poner arriba el reloadTweets, cada vez que cambie ese estado se renderiza la página
+const deleteTweet = (i) => {
+  allTweets.splice(i, 1);
+  setAllTweets(allTweets);
+  //Transformamos allTweets, que es un objeto,en una cadena de texto JSON. Setitem para guardar en ls
+  localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweets));
+  console.log(JSON.stringify(allTweets))
+  // hacemos que se recargue la página cuando se elimina un Tweet
+  setReloadTweets(true)
+
+}
 
   return (
    <Container className='tweets-simulator' maxWidth={false} >
      <Header />
      <SendTweet setToastProps={setToastProps} allTweets={allTweets} />
+     <ListTweets allTweets={allTweets} deleteTweet={deleteTweet}/>
      <Snackbar
      anchorOrigin={{
        vertical: 'top',
