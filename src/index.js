7382a5c33efdaf0a36bbb109b1424887e432da04
IreneBioque <irenebioque@gmail.com>
2022-01-18 20:47:48 +0100

    change index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter>
-        <App />
-    </HashRouter> ,  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
